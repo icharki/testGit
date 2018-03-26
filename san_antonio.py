b97706ca5bd16960704f52b5c7d944833df4c386
Ilias Charki <ilias.charki.pro@gmail.com>
2018-03-26 23:27:30 +0200

    J\'ai passé les items des listes en une seule ligne

--- san_antonio.py
+++ san_antonio.py
@@ -2,20 +2,8 @@
 
 import random
 
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
-
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero", 
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
 
 def show_random_quote(my_list):
   rand_namb = random.randint(0, len(my_list) - 1)
