1e3638f41fcb57c4e2cbf07be0bec4638ec7eeae
Ilias Charki <ilias.charki.pro@gmail.com>
2018-03-26 23:28:14 +0200

    J'ai passé les items des listes en une seule ligne

--- san_antonio.py
+++ san_antonio.py
@@ -3,6 +3,7 @@
 import random
 
 quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
+
 characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
 
 def show_random_quote(my_list):
